fc925783f63850f1773cf933174ad1b40c851934
Lujia4170 <wanglj@bu.edu>
2019-10-10 22:29:40 -0400

    Update test-Isorank.R

--- tests/testthat/test-Isorank.R
+++ tests/testthat/test-Isorank.R
@@ -7,16 +7,17 @@ A <- cgnp_pair$graph1
 B <- cgnp_pair$graph2
 startm <- matrix(0, 10, 10)
 diag(startm)[1:4] <- 1
-
+seeds<-1:4
 
 test_that("matching correspondence between graph1 and graph2", {
-  expect_equal(graph_match_IsoRank(A, B,startm, alpha = .3, method = "greedy")$corr,
-               data.frame(corr_A = c(3,4,1,2,9,5,8,7,10,6), corr_B = c(3,4,1,2,5,9,8,10,7,6)))
+  expect_equal(graph_match_IsoRank(A, B,startm, seeds,alpha = .3, method = "greedy")$corr,
+               data.frame(corr_A = c(1:10), corr_B = c(1,2,3,4,9,6,10,8,5,7),row.names=as.character(1:10)))
 })
 test_that("order of nodes getting matched", {
-  expect_equal(graph_match_IsoRank(A, B,startm, alpha = .3, method = "greedy")$order,
-               c(3,4,1,2,6,10,8,7,5,9))
+  expect_equal(graph_match_IsoRank(A, B,startm,seeds, alpha = .3, method = "greedy")$order,
+               c(1,2,3,4,6,10,8,7,5,9))
 })
 test_that("test number of seeds", {
-  expect_equal(graph_match_IsoRank(A, B,startm, alpha = .3, method = "greedy")$ns,0)
+  expect_equal(graph_match_IsoRank(A, B,startm, seeds,alpha = .3, method = "greedy")$ns,4)
 })
+
