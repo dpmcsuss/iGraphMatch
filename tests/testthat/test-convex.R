f595ec35ae378dd81426e4241745d2db9eda426d
Daniel Sussman <sussman@bu.edu>
2020-05-16 07:46:24 -0400

    very basic convex testing

--- tests/testthat/test-convex.R
+++ tests/testthat/test-convex.R
@@ -1,29 +1,24 @@
-# context("Convex")
+context("Convex")
 
-# # sample pair of graphs w. 10 vertices
-# set.seed(123)
-# cgnp_pair <- sample_correlated_gnp_pair(n=10, corr=0.3, p=0.5)
-# A <- cgnp_pair$graph1
-# B <- cgnp_pair$graph2
-# seeds <- 1:10 <= 3
+# sample pair of graphs w. 10 vertices
+set.seed(123)
+cgnp_pair <- sample_correlated_gnp_pair(n=10, corr=0.8, p=0.5)
+A <- cgnp_pair$graph1
+B <- cgnp_pair$graph2
 
 # ex_df <- data.frame(corr_A = c(1:10),
-#           corr_B = c(4, 2, 9, 8, 5, 7, 10, 6, 3, 1))
+          # corr_B = c(4, 2, 9, 8, 5, 7, 10, 6, 3, 1))
 
-# actual <- graph_match_convex(A, B)
+actual <- graph_match_convex(A, B)
 
-# test_that("matching correspondence between graph1 and graph2",
-#   {
-#     expect_equal(actual$corr, ex_df)
-#   })
-# test_that("permutation matrix", {
-#   expect_equal(actual$P,
-#     get_perm(10, 10, ex_df))
-# })
-# test_that("number of seeds", {
-#   expect_equal(actual$ns,0)
-# })
-
-
-
-
+test_that("matching correspondence between graph1 and graph2",
+  {
+    expect_equal(length(actual), 6)
+  })
+test_that("doubly stochastic", {
+  expect_lt(sum(abs(rowSums(actual$D) - 1)), 10e-6)
+  expect_lt(sum(abs(colSums(actual$D) - 1)), 10e-6)
+})
+test_that("number of seeds", {
+  expect_equal(actual$ns,0)
+})
