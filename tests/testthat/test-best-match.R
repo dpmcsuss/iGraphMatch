14ac9737406b592957235ba9eec10925a93bb293
 Qzh <qiaozihuan@hotmail.com>
2020-10-25 00:35:11 -0400

    cleaner input & mult-layer

--- tests/testthat/test-best-match.R
+++ tests/testthat/test-best-match.R
@@ -1,30 +1,18 @@
 context("best matches ranking")
 
 # sample pair of graphs w. 10 vertices
-g <- sample_correlated_gnp_pair(n = 10, corr = 0.4, p = 0.5)
+set.seed(123)
+g <- sample_correlated_gnp_pair(n = 10, corr = 0.6, p = 0.5, directed = TRUE)
 A <- g$graph1
 B <- g$graph2
-seeds <- 1:10 <= 3
-nonseeds <- !seeds
-match_corr <- graph_match_FW(A, B, seeds, start = "bari")$corr
+seeds <- 1:3
+match <- graph_match_percolation(A, B, seeds, r = 3)
 
 test_that("best matches for all matches using all measures", {
-  expect_equal(nrow(best_matches(A, B, measure = "row_cor", 
-                                         num = 3, x = nonseeds, match_corr)), 3)
-  expect_equal(nrow(best_matches(A, B, measure = "row_diff", 
-                                         num = 3, x = nonseeds, match_corr)), 3)
-  expect_equal(nrow(best_matches(A, B, measure = "row_perm_stat", 
-                                         num = 3, x = nonseeds, match_corr)), 3)
-})
-
-cor_true <- data.frame(A_best = c(5,6,7), B_best = c(9,6,7))
-diff_true <- data.frame(A_best = c(5,4,6), B_best = c(9,10,6))
-perm_true <- cor_true
-test_that("best matches for partial match result",{
-  expect_equal(nrow(best_matches(A, B, measure = "row_cor", num = 3, x = nonseeds, 
-                            match_corr[1:8,])), 3)
-  expect_equal(nrow(best_matches(A, B, measure = "row_diff", num = 3, x = nonseeds, 
-                            match_corr[1:8,])), 3)
-  expect_equal(nrow(best_matches(A, B, measure = "row_perm_stat", num = 3, x = nonseeds, 
-                            match_corr[1:8,])), 3)
+  expect_equal(nrow(best_matches(A, B, match, measure = "row_cor", 
+                                         num = 4)), 4)
+  expect_equal(nrow(best_matches(A, B, match, measure = "row_diff", 
+                                 num = 4)), 4)
+  expect_equal(nrow(best_matches(A, B, match, measure = "row_perm_stat", 
+                                 num = 4)), 4)
 })
