929882ceca20b9d74977df3a4a23d977fb9340b9
Daniel Sussman <sussman@bu.edu>
2020-12-21 12:10:13 -0500

    Fix match_plot issues

--- R/match_report.R
+++ R/match_report.R
@@ -261,11 +261,14 @@ match_plot_igraph <- function(A, B, match,
   igraph::E(g)$lty <- 1
   if (color) {
     igraph::E(g)$color <- 
-      pal[as.numeric(as.factor(igraph::E(g)$edge_match))]
+      pal[as.numeric(as.factor(igraph::E(g)$edge_match,
+        levels = c("Both", "Only A", "Only B")))]
+    ))]
   }
   if (linetype) {
     igraph::E(g)$lty <- 
-      as.numeric(as.factor(igraph::E(g)$edge_match))
+      as.numeric(as.factor(igraph::E(g)$edge_match,
+        levels = c("Both", "Only A", "Only B")))
   }
 
   plot(g, ...)
