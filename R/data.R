f22df35ddf67b5492cee67033b153e7b3d38625f
ZihuanQiao <zihuanqiao@hotmail.com>
2021-01-07 00:09:23 -0500

    add Transportation data

--- R/data.R
+++ R/data.R
@@ -11,7 +11,12 @@
 #' the first network in the list is the chemical synapses network and the
 #' other one is the electrical synapses network.
 #'
-#'
+#' @usage data(C.Elegans)
+#' 
+#' @examples 
+#' data(C.Elegans)
+#' g1 <- C.Elegans[[1]]
+#' g2 <- C.Elegans[[2]]
 #' 
 "C.Elegans"
 
@@ -28,6 +33,44 @@
 #' Two email communication networks for two different weeks are stored in a
 #' list in the form of igraph objects.
 #'
-#'
+#' @usage data(Enron)
+#' 
+#' #' @examples 
+#' data(Enron)
+#' g1 <- Enron[[1]]
+#' g2 <- Enron[[2]]
 #' 
 "Enron"
+
+
+
+#' Britain Transportation network
+#' 
+#' The network reflects the transportation connections in the UK, with five 
+#' layers representing ferry, rail, metro, coach, and bus.
+#' The original graph has 262385 nodes and 461555 connections in five layers in 
+#' total.
+#' A smaller graph was constructed based on a random walk starting from a 
+#' randomly chosen hub node, a node that has connections in all the layers.
+#' The template graph has 53 nodes and 56 connections in total and is an induced 
+#' subgraph of the original graph.
+#' In addition, the dataset also contains a list of candidate matches for each 
+#' template node, where the true correspondence is guaranteed to among the candidates.
+#' The world graph, the template graph and the candidate matches information are stored 
+#' in a list where two graphs are stored in two lists of igraph objects representing
+#' different layers, and the candidate information is stored in a data frame where the 
+#' ids of the template nodes and the world nodes are in columns named "tem" and "wor".
+#'
+#' @references Gallotti Riccardo and Barthelemy Marc. (2015), \emph{The Multilayer 
+#' Temporal Network of Public Transport in Great Britain}. Scientific Data. 
+#' 
+#' @usage 
+#' data(Transportation)
+#' 
+#' @examples 
+#' data(Transportation)
+#' template <- Transportation[[1]]
+#' world <- Transportation[[2]]
+#' candidate <- Transportation[[3]]
+#' 
+"Transportation"
